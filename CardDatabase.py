469d4d2eaf8ff3e5e8611142ebb4a95acc51de7b
Dr. Aaron Knight <a.knight14@hotmail.com>
2021-09-09 21:02:01 +0100

    Adding method to create individual series info along with a method to clean the name of the series for the db

--- CardDatabase.py
+++ CardDatabase.py
@@ -12,15 +12,30 @@ class CardDatabase():
         self._conn = sqlite3.connect(self.database)
         self._cur = self._conn.cursor()
 
-    def create_test_db(self):
+    def create_deck_info_db(self):
 
         self._cur.execute('''CREATE TABLE IF NOT EXISTS deck_info
                              (year_released date,
                               series_name text,
                               num_cards integer)''')
 
+    def create_series_info_db(self, name_of_series):
+
+        name_of_series = self.clean_text(name_of_series)
+        
+        self._cur.execute('''CREATE TABLE IF NOT EXISTS %s
+                             (number integer,
+                              pokemon text,
+                              card_type text,
+                              owned text)''' % name_of_series)
+
+    def clean_text(self, text):
+
+        text = text.replace(" ", "_").lower()
+        return text
 
 if __name__ == "__main__":
 
     db = CardDatabase("test.db")
-    db.create_test_db()
+    db.create_deck_info_db()
+    db.create_series_info_db("TEST DB")
